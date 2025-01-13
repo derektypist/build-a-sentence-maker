383023d1fab4229c31074fd367af24440c7d728b
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2025-01-13 12:19:58 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let adjective = "blue";
+let noun = "bird";
+let verb = "snowing";
+let place = "nest";
+let adjective2 = "orange";
+let noun2 = "peanuts";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// Reassign variables
+adjective = "white";
+noun = "panda";
+verb = "raining";
+place = "zoo";
+adjective2 = "black";
+noun2 = "bamboo";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
